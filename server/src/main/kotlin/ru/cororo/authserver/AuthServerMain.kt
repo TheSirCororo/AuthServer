86358ca3dd38709822c81e4aac520939805b083c
TheSirCororo <artem.cororo@gmail.com>
2022-10-12 16:25:51 +0300

    fixes

--- server/src/main/kotlin/ru/cororo/authserver/AuthServerMain.kt
+++ server/src/main/kotlin/ru/cororo/authserver/AuthServerMain.kt
@@ -1,4 +1,5 @@
 @file:JvmName("AuthServerMain")
+
 package ru.cororo.authserver
 
 /**
--- server/src/main/kotlin/ru/cororo/authserver/world/chunk/MinecraftChunk.kt
+++ server/src/main/kotlin/ru/cororo/authserver/world/chunk/MinecraftChunk.kt
@@ -4,5 +4,6 @@ class MinecraftChunk(
     override val x: Int,
     override val z: Int
 ) : Chunk {
+    override val sections = mutableListOf<ChunkSection>()
 
 }
